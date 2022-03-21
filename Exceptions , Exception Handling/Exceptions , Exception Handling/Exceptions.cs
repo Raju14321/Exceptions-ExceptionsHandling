df4bc8bdaa9a21e0d3120ff76b99f82e019f5a1b
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-22 00:07:33 +0530

    Add files via upload

---
+++ Exceptions , Exception Handling/Exceptions , Exception Handling/Exceptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exceptions___Exception_Handling
+{
+    class Exceptions
+    {
+       static void Main()
+       {
+            Console.Write("Ënter 1st number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Ënter 2nd number:");
+            int y = int.Parse(Console.ReadLine());
+           
+            int z = x / y;
+            Console.WriteLine("The result is:" + z);
+            Console.WriteLine("Ënd of the program");
+            Console.WriteLine("--------------------------------------");
+            try
+            {
+                Console.Write("Ënter 1st number:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Ënter 2nd number:");
+                int b = int.Parse(Console.ReadLine());
+              
+                int c = a / b;
+                Console.WriteLine("The result is:" + c);
+
+            }
+            catch (DivideByZeroException ex1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Attempted to divided by zero");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            catch (FormatException ex2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Input must be numaric");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ex.Message");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+                Console.WriteLine("Ënd of the program");
+
+            }
+
+       } 
+    
+    }
+}
---
+++ Exceptions , Exception Handling/Exceptions , Exception Handling/ThrowDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions___Exception_Handling
+{
+    internal class ThrowDemo
+    {
+        static void Main()
+        {
+            Console.Write("Ënter 1st number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Ënter 2nd number:");
+            int y = int.Parse(Console.ReadLine());
+            if(y%2>0)
+            {
+
+                throw new ApplicationException("Division should not be a Odd Number.");
+                
+            }
+            int z = x / y;
+            Console.WriteLine("The result is:" + z);
+            Console.WriteLine("Ënd of the program");
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+}
